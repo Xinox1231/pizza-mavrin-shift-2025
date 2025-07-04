0ec9496dc1b714a564dd9971cb57842ceab515c6
Xinox3213 <mavrinonlywork@gmail.com>
2025-07-04 18:46:12 +0700

    AuthorizationComponent теперь выделен в отдельный флоу. Авторизация разбита на 2 экрана, чтобы улучшить пользовательский опыт

--- app/src/main/java/ru/mavrinvladislav/shifttask2025/authorization/presentation/AuthorizationContent.kt
+++ app/src/main/java/ru/mavrinvladislav/shifttask2025/authorization/presentation/AuthorizationContent.kt
@@ -1,7 +1,28 @@
 package ru.mavrinvladislav.shifttask2025.authorization.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
+import com.arkivanov.decompose.extensions.compose.jetpack.subscribeAsState
+import ru.mavrinvladislav.shifttask2025.authorization.presentation.otp_input_screen.OtpInputScreen
+import ru.mavrinvladislav.shifttask2025.authorization.presentation.phone_input_screen.PhoneInputScreen
 
 @Composable
 fun AuthorizationContent(component: AuthorizationComponent) {
+
+    val navigation by component.navStack.subscribeAsState()
+
+    Children(
+        stack = navigation
+    ) {
+        when (val instance = it.instance) {
+            is AuthorizationChild.OtpInput -> {
+                OtpInputScreen(instance.component)
+            }
+
+            is AuthorizationChild.PhoneInput -> {
+                PhoneInputScreen(instance.component)
+            }
+        }
+    }
 }
