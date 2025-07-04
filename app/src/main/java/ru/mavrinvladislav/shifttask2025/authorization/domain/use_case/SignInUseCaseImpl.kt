c52d2e5cd9573bbb1e96280332960b0c3f4d9eb1
Xinox3213 <mavrinonlywork@gmail.com>
2025-07-04 18:41:52 +0700

    Убрал Either

--- app/src/main/java/ru/mavrinvladislav/shifttask2025/authorization/domain/use_case/SignInUseCaseImpl.kt
+++ app/src/main/java/ru/mavrinvladislav/shifttask2025/authorization/domain/use_case/SignInUseCaseImpl.kt
@@ -1,12 +1,11 @@
 package ru.mavrinvladislav.shifttask2025.authorization.domain.use_case
 
 import ru.mavrinvladislav.shifttask2025.authorization.domain.repository.AuthorizationRepository
-import ru.mavrinvladislav.shifttask2025.core.common.util.Either
 import javax.inject.Inject
 
 class SignInUseCaseImpl @Inject constructor(
     private val repository: AuthorizationRepository
 ) : SignInUseCase {
-    override suspend fun invoke(phoneNumber: String, otpCode: String): Either<Unit, String> =
+    override suspend fun invoke(phoneNumber: String, otpCode: String) =
         repository.signIn(phoneNumber, otpCode)
 }
